93e7046b777e2c3f89cc0867510d22f71ac669ca
Srdjan Rakic <srgjan.rakic@gmail.com>
2018-05-14 13:39:06 +0200

    Remove unused components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Todo from "./Todo"
+import Todo from './Todo';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Todo />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Todo />
+  </Provider>,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
